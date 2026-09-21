c45c82b0e57f152041ddae098ac3be11052d385e
ygtcnmrc <yigitcan.meric@icloud.com>
2026-09-22 00:42:27 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='barskt-v7';
+const CACHE='barskt-v8-2';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{ if(e.request.method==='GET' && new URL(e.request.url).origin===location.origin){e.respondWith(caches.open(CACHE).then(c=>c.match(e.request).then(r=>r||fetch(e.request).then(x=>{c.put(e.request,x.clone());return x}))))} });
